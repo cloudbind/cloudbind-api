815c8460542999c1827516239f3ed3510a2c9893
blackchapel <kc@protonmail.com>
2022-12-14 18:45:22 +0530

    imported routed

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ const db = require('./configs/connection');
 
 // Importing Routes
 const authRoutes = require('./routes/auth.route');
-const fileRoutes = require('./routes/file.route')
+const fileRoutes = require('./routes/file.route');
+const userRoutes = require('./routes/user.route');
+const friendRoutes = require('./routes/friend.route');
 
 // Initializing an express app
 const app = express();
@@ -26,16 +28,19 @@ app.use(morgan('dev'));
 // API Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/upload', fileRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/friend', friendRoutes);
 
 // Test API
 app.get('/api', (req, res) => {
-	res.status(200).json({
-    	name: `${process.env.APP_NAME}`,
-    	apiVersion: JSON.parse(fs.readFileSync('./package.json').toString()).version
-  	});
+    res.status(200).json({
+        name: `${process.env.APP_NAME}`,
+        apiVersion: JSON.parse(fs.readFileSync('./package.json').toString())
+            .version
+    });
 });
 
 // Listening on the port
 app.listen(PORT, () => {
-	console.log(`Server listening on port ${PORT}`);
+    console.log(`Server listening on port ${PORT}`);
 });
