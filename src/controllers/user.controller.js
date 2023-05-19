e60517c3b6e39fdfe330165b52e6a5087054f3f8
blackchapel <kc.dark@protonmail.com>
2023-05-20 00:55:27 +0530

    tag generation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+const { cloudinary } = require('./../utilities/utils');
 const User = require('../models/user.schema');
 
 const searchUsers = async (req, res) => {
@@ -68,7 +70,56 @@ const visibilitySetting = async (req, res) => {
     }
 };
 
+const generateTag = async (req, res) => {
+    try {
+        if (req.files) {
+            const user = await User.findById(req.user.id);
+
+            if (!user) {
+                res.status(404).json({
+                    message: 'user not found'
+                });
+            } else {
+                const fileUrlCloudinary = await cloudinary.uploader.upload(
+                    req.files['file'][0].path,
+                    {
+                        public_id: `cloudbind/${req.files['file'][0].filename}`
+                    }
+                );
+
+                const response = await axios.post(
+                    'https://senpai3003.pythonanywhere.com/face',
+                    {
+                        url: fileUrlCloudinary.url
+                    }
+                );
+
+                user.tag = parseInt(response[0].tag);
+                await user.save();
+
+                // Delete file from server
+                fs.unlinkSync(req.files['file'][0].path);
+
+                res.status(201).json({
+                    message: 'Tag generated',
+                    data: user
+                });
+            }
+        } else {
+            res.status(400).json({
+                message: 'Please attach file'
+            });
+        }
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({
+            message: error.message
+        });
+    }
+};
+
 module.exports = {
     searchUsers,
-    visibilitySetting
+    visibilitySetting,
+    generateTag
 };
